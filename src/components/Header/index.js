2de0e81e3fd0d36d5f267d822bb27a46fd8d2278
Alexander Rau <alexanderrau86@yahoo.com>
2019-02-18 12:59:33 -0700

    Build Header and Sidebar components initial builds

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export class Header extends Component {
+  constructor() {
+    super()
+  }
+  
+  render() {
+    return(
+      <div>
+        <h1>Alexander Rau</h1>
+        <h2>Front-End Web Developer</h2>
+      </div>
+    )
+  }
+}
+
+export default Header
