39de61d507bd5035cabc5ecf18160abebd142a3f
Alexander Rau <alexanderrau86@yahoo.com>
2019-02-24 16:36:33 -0700

    add contact icons and more css to contacts components

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './Contact.css';
-import pdf from '../../utils/assets/Alexander-Rau-Resume.pdf'
+import pdf from '../../utils/assets/Alexander-Rau-Resume.pdf';
+import resume from '../../utils/assets/icons/004-resume.svg';
+import github from '../../utils/assets/icons/002-github.svg';
+import linkedin from '../../utils/assets/icons/linkedin-sign.svg';
+import email from '../../utils/assets/icons/003-close-envelope.svg';
 
 export class Contact extends Component {
   constructor() {
@@ -12,26 +16,42 @@ export class Contact extends Component {
       <div className='contact-container'>
         <h1>Contact Info</h1>
         <div className='contact-links'>
-          <a
-            href={pdf}
-            target='_blank'
-            rel='noopener noreferrer'
-          >Resume</a>
-          <a 
-            href='https://github.com/raualex'
-            target='_blank'
-            rel='noopener noreferrer'
-          >GitHub</a>
-          <a
-            href='https://www.linkedin.com/in/alexander-rau/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >LinkedIn</a>
-          <a 
-            href='mailto:raualexander86@gmail.com'
-            target='_blank'
-            rel='noopener noreferrer'
-          >Email</a>        
+          <div className='link-container'>
+            <a
+              href={pdf}
+              target='_blank'
+              rel='noopener noreferrer'
+              className='resume-link'
+            >Resume</a>
+            <img src={resume} alt='resume icon' className='contact-icon' />
+          </div>
+          <div className='link-container'>
+            <a 
+              href='https://github.com/raualex'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='github-link'
+            >GitHub</a>
+            <img src={github} alt='github icon' className='contact-icon' />
+          </div>
+          <div className='link-container'>
+            <a
+              href='https://www.linkedin.com/in/alexander-rau/'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='linkedin-link'
+            >LinkedIn</a>
+            <img src={linkedin} alt='linked in icon' className='contact-icon' />
+          </div>
+          <div className='link-container'>
+            <a 
+              href='mailto:raualexander86@gmail.com'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='email-link'
+            >Email</a>
+            <img src={email} alt='email icon' className='contact-icon' />
+          </div>
         </div>
       </div>
     )
