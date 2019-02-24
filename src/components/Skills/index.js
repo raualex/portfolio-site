b2347607fd79cfff046caa547eb3a90ec60ba315
Alexander Rau <alexanderrau86@yahoo.com>
2019-02-23 23:50:55 -0700

    Write out separate CSS for each skill logo

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -33,39 +33,39 @@ export class Skills extends Component {
         <div className='skills-box'>
           <div className='front-end-skills'>
             <h1>Front-End</h1>
-            <img src={react} alt='react logo' className='program-logo' />
-            <img src={redux} alt='redux logo' className='program-logo' />
-            <img src={jshtmlcss} alt='javscript html and css logo' className='program-logo' />
-            <img src={jquery} alt='j query logo' className='program-logo' />
+            <img src={react} alt='react logo' className='react-logo' />
+            <img src={redux} alt='redux logo' className='redux-logo' />
+            <img src={jshtmlcss} alt='javscript html and css logo' className='htmlcssjs-logos' />
+            <img src={jquery} alt='j query logo' className='jquery-logo' />
           </div>
           <div className='back-end-skills'>
             <h1>Back-End</h1>
-            <img src={node} alt='node logo' className='program-logo' />
-            <img src={express} alt='express logo' className='program-logo' />
-            <img src={knex} alt='knex logo' className='program-logo' />
-            <img src={postgresql} alt='postgres logo' className='program-logo' />
-            <img src={microsoftsql} alt='microsoft sql server logo' className='program-logo' />
+            <img src={node} alt='node logo' className='node-logo' />
+            <img src={express} alt='express logo' className='express-logo' />
+            <img src={knex} alt='knex logo' className='knex-logo' />
+            <img src={postgresql} alt='postgres logo' className='postgresql-logo' />
+            <img src={microsoftsql} alt='microsoft sql server logo' className='mssql-logo' />
           </div>
           <div className='testing-skills'>
             <h1>Testing</h1>
-            <img src={chaimocha} alt='chai and mocha logos' className='program-logo' />
-            <img src={enzymejest} alt='enzyme and jest logos' className='program-logo' />
-            <img src={travis} alt='Travis CI logo' className='program-logo' />
+            <img src={chaimocha} alt='chai and mocha logos' className='chaimocha-logos' />
+            <img src={enzymejest} alt='enzyme and jest logos' className='enzymejest-logos' />
+            <img src={travis} alt='Travis CI logo' className='travisci-logo' />
           </div>
           <div className='workflow-skills'>
             <h1>Workflow/Tools</h1>
-            <img src={git} alt='git logo' className='program-logo' />
-            <img src={github} alt='github logo' className='program-logo' />
-            <img src={heroku} alt='heroku logo' className='program-logo' />
-            <img src={waffleio} alt='waffle IO logo' className='program-logo' />
-            <img src={jira} alt='jira logo' className='program-logo' />
+            <img src={git} alt='git logo' className='git-logo' />
+            <img src={github} alt='github logo' className='github-logo' />
+            <img src={heroku} alt='heroku logo' className='heroku-logo' />
+            <img src={waffleio} alt='waffle IO logo' className='waffleio-logo' />
+            <img src={jira} alt='jira logo' className='jira-logo' />
           </div>
           <div className='currently-learning-skills'>
             <h1>Currently Learning</h1>
-            <img src={ember} alt='ember logo' className='program-logo' />
-            <img src={vue} alt='vue logo' className='program-logo' />
-            <img src={angular} alt='angular logo' className='program-logo' />
-            <img src={cucumber} alt='cucumber logo' className='program-logo' />
+            <img src={ember} alt='ember logo' className='ember-logo' />
+            <img src={vue} alt='vue logo' className='vue-logo' />
+            <img src={angular} alt='angular logo' className='angular-logo' />
+            <img src={cucumber} alt='cucumber logo' className='cucumber-logo' />
           </div>
         </div>
       </div>
