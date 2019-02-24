29b5bacf8d5b013d8892c555abb0b71d60f865cb
Alexander Rau <alexanderrau86@yahoo.com>
2019-02-24 15:20:07 -0700

    Build out contact links

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Contact.css';
+import pdf from '../../utils/assets/Alexander-Rau-Resume.pdf'
 
 export class Contact extends Component {
   constructor() {
@@ -9,7 +10,29 @@ export class Contact extends Component {
   render() {
     return(
       <div className='contact-container'>
-        <h1>My Contact Info!</h1>
+        <h1>Contact Info</h1>
+        <div className='contact-links'>
+          <a
+            href={pdf}
+            target='_blank'
+            rel='noopener noreferrer'
+          >Resume</a>
+          <a 
+            href='https://github.com/raualex'
+            target='_blank'
+            rel='noopener noreferrer'
+          >GitHub</a>
+          <a
+            href='https://www.linkedin.com/in/alexander-rau/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >LinkedIn</a>
+          <a 
+            href='mailto:raualexander86@gmail.com'
+            target='_blank'
+            rel='noopener noreferrer'
+          >Email</a>        
+        </div>
       </div>
     )
   }
